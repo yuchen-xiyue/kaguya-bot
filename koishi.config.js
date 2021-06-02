307e6fa59aba9f7d14528b387479171c7d3b8b3c
17205961 <yuchen.wu@ucdconnect.ie>
2021-06-02 20:05:24 +0800

    add cqhttp

--- koishi.config.js
+++ koishi.config.js
@@ -1,26 +1,26 @@
 // 配置项文档：https://koishi.js.org/api/app.html
 module.exports = {
   // Koishi 服务器监听的端口
-  port: 8000,
+  port: 8080,
   onebot: {
     // 对应 cqhttp 配置项 http_config.post_urls, ws_reverse_servers.reverse_url
-    path: '',
-    secret: '',
+    path: '/kaguya-bot',
+    secret: 'password',
   },
   bots: [{
     type: 'onebot:http',
     // 对应 cqhttp 配置项 http_config.port
     server: 'http://localhost:5700',
-    selfId: 827901313,
+    selfId: '827901313',
     token: '',
   }],
   plugins: {
     mysql: {
-      host: '127.0.0.1',
+      host: '172.21.0.16',
       // Koishi 服务器监听的端口
       port: 3306,
       user: 'root',
-      password: '',
+      password: '1101011999@Ww',
       database: 'koishi',
     },
     common: {},
